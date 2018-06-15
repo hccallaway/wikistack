f39337309fa3174a50f10d15196c95919e5143f5
Hadley Callaway <hccallaway@gmail.com>
2018-06-15 12:24:20 -0400

    sequelize stuff

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 
-const layout = require('./views/layout')
-const { db } = require('./models');
+const layout = require('./views/layout');
+const { db, Page, User } = require('./models');
+
+// const db = new Sequelize('postgres://localhost:5432/wikistack', {
+//     logging: false
+// });
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
-db.authenticate().
-then(() => {
+db.authenticate().then(() => {
   console.log('connected to the database');
-})
+});
 
 app.get('/', (req, res, next) => {
-    res.send(layout())
-})
+  res.send(layout());
+});
 
-const PORT = 1337;
+const init = async () => {
+  await db.sync({ force: true });
+  const PORT = 1337;
+  app.listen(PORT, () => {
+    console.log(`listening in on ${PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-    console.log(`listening in on ${PORT}`)
-})
+init();
